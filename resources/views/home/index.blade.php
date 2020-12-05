9705ecd0422121a9b392fe504eeb3aa68a0abf42
byznryldrm <byznryldrm2@gmail.com>
2020-12-06 00:54:51 +0300

    uygulama olusturuldu, ilk controller ve view'lar test edildi, index'in içerisine route'dan yonlendirmeler yapildi.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
 <h1>Welcome to Laravel</h1>
 <p>This is a paragraph.</p>
 <br>
-<a href="{{route('test', ['id' => 1, 'name' => 'Beyza'])}}">Test Sayfa</a>
+<a href="{{route('test', ['id' => 1, 'name' => 'Beyza'])}}">Test Sayfa 234</a>
 
 </body>
 </html>
